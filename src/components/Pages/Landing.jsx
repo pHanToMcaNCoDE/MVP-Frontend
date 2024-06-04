dc9ba07e6844d8b09bb0435659ffc4711d995c98
SamsonI95 <samsoniweibo@gmail.com>
2024-06-04 09:43:19 +0100

    Waitlist code integration

--- src/components/Pages/Landing.jsx
+++ src/components/Pages/Landing.jsx
@@ -40,7 +40,6 @@ const Landing = () => {
     try {
       const response = await axios(config);
       console.log(JSON.stringify(response.data));
-      alert("Successfully joined the waitlist!")
     } catch (error) {
       console.log(error);
     }
