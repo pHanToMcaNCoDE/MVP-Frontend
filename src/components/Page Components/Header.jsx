cc8a2959ce62302d4fbc8c4584acf9d53a7bb636
SamsonI95 <samsoniweibo@gmail.com>
2024-06-09 09:57:01 +0100

    Sign up button test

--- src/components/Page Components/Header.jsx
+++ src/components/Page Components/Header.jsx
@@ -11,6 +11,9 @@ const Header = () => {
       <Link to="/home" className="flex items-center justify-center sm:py-[16px] py-[24px]">
         <img src={HeaderLogo} alt="logo-icon" />
       </Link>
+      <Link to="/signup" className="">
+        <button>Sign Up</button>
+      </Link>
     </div>
   );
 };
