6c036b49a242f9529aafcf88f9b2b48e7939aded
phantomcancode <vodumeh@gmail.com>
2024-06-17 02:38:03 +0100

    implementing the body of the dashboard

--- src/components/Pages/AdminDashboard/Dashboard/Dashboard.jsx
+++ src/components/Pages/AdminDashboard/Dashboard/Dashboard.jsx
@@ -21,17 +21,17 @@ const Dashboard = () => {
 
         <div className='flex justify-center items-start gap-[16px]'>
             <button className='px-2 py-0 w-[140px] h-[40px] bg-[#2F4EED] flex justify-center items-center text-white rounded-[50px]'>
-              Send <RxArrowTopRight />
+              <p className='px-2'>Send</p> <RxArrowTopRight />
             </button>
             <button className='px-2 py-0 w-[140px] h-[40px] bg-[#2F4EED] flex justify-center items-center text-white rounded-[50px]'>
-              Receive <RxArrowBottomLeft />
+              <p className='px-2'>Receive</p> <RxArrowBottomLeft />
             </button>
         </div>
       </div>
-      <div className='py-[16px] px-0 gap-[10px] flex flex-col justify-center items-start self-stretch border-b border-[#E9E9E9]'>
-        <div className='gap-[24px] flex items-start px-[32px] py-0 self-stretch'>
-          <h1>Assets</h1>
-          <h1>Transactions</h1>
+      <div className='py-[16px] px-0 gap-[10px] flex flex-col justify-center items-start self-stretch'>
+        <div className='gap-[24px] flex items-start px-[32px] py-0 self-stretch border-b border-[#E9E9E9]'>
+          <h1 className='text-[#9C9C9C] text-[1rem] font-semibold leading-6 gap-2 pb-2'>Assets</h1>
+          <h1 className='text-[#9C9C9C] text-[1rem] font-semibold leading-6 gap-2 pb-2'>Transactions</h1>
         </div>
       </div>
       <div className='pt-[16px] flex items-start self-stretch w-full'>
