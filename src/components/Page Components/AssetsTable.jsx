cb03c04af124031d761f9cb86027e737e9f7729c
phantomcancode <vodumeh@gmail.com>
2024-06-17 02:22:50 +0100

    implementing the body of the dashboard

---
+++ src/components/Page Components/AssetsTable.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const AssetsTable = () => {
+  return (
+    <table className='flex'>
+        <tr>
+            <th className='pt-0 pl-[32px] pb-[16px] pr-[10px] flex items-center self-stretch gap-[10px] border-b border-[#E9E9E9]'>Token</th>
+            <th className='pt-0 pl-[32px] pb-[16px] pr-[10px] flex items-center self-stretch gap-[10px] border-b border-[#E9E9E9]'>Price</th>
+            <th className='pt-0 pl-[32px] pb-[16px] pr-[10px] flex items-center self-stretch gap-[10px] border-b border-[#E9E9E9]'>Balance</th>
+        </tr>
+        <tbody>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+  )
+}
+
+export default AssetsTable
